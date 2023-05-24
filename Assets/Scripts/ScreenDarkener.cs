5428196e6b3e1b75b3d6d4a02615af395d7d3e19
Carlton Mpofu <mpofucarlton0@gmail.com>
2023-05-24 23:20:00 +0200

    Complete animation for when player chnages dimension

---
+++ Assets/Scripts/ScreenDarkener.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenDarkener : MonoBehaviour
+{
+    // public float initialAlpha = 1f;
+    // public float targetAlpha = 0f;
+
+    public float initialAlpha = 0f;
+    public float targetAlpha = 1f;
+    public float startAnimationDuration = 2f;
+    public float endAnimationDuration = 1f;
+
+    private Image image;
+    private float currentAlpha;
+    private float timer;
+
+    public bool makeScreenDark;
+    public bool makeScreenLight;
+
+    SwitchDimension switchDimension;
+    Animator myAnimator;
+
+    private void Start()
+    {
+        switchDimension = FindObjectOfType<SwitchDimension>();
+        myAnimator = FindObjectOfType<Animator>();
+
+        makeScreenDark = false;
+        makeScreenLight = false;
+        image = GetComponent<Image>();
+        currentAlpha = initialAlpha;
+        //image.color = new Color(0f, 0f, 0f, currentAlpha);
+        
+    }
+
+    private void Update()
+    {
+        if(makeScreenDark)
+        {
+            timer += Time.deltaTime;
+
+            // Calculate the current alpha value based on the timer and animation duration
+            currentAlpha = Mathf.Lerp(initialAlpha, targetAlpha, timer / startAnimationDuration);
+
+            // Set the new alpha value for the Image
+            image.color = new Color(0f, 0f, 0f, currentAlpha);
+
+            if(timer >= startAnimationDuration)
+            {
+                makeScreenDark = false;
+                switchDimension.ChangeDimension();
+
+                StartLighteningAnimation();
+            }
+        }
+
+        if(makeScreenLight)
+        {
+            timer += Time.deltaTime;
+
+            // Calculate the current alpha value based on the timer and animation duration
+            currentAlpha = Mathf.Lerp(initialAlpha, targetAlpha, timer / endAnimationDuration);
+
+            // Set the new alpha value for the Image
+            image.color = new Color(0f, 0f, 0f, currentAlpha);
+
+            if(timer >= endAnimationDuration)
+            {
+                makeScreenLight = false;
+                switchDimension.SetInLightDimension();
+            }
+        }
+    }
+
+    public void StartDarkeningAnimation()
+    {
+        initialAlpha = 0f;
+        targetAlpha = 1f;
+
+        // Reset the timer and set the initial alpha value
+        timer = 0f;
+        currentAlpha = initialAlpha;
+
+        makeScreenDark = true;
+        myAnimator.SetBool("changeDimension", true);
+    }
+
+    private void StartLighteningAnimation()
+    {
+        initialAlpha = 1f;
+        targetAlpha = 0f;
+        image.color = new Color(1f, 1f, 1f, initialAlpha);
+
+        timer = 0f;
+        currentAlpha = initialAlpha;
+        makeScreenLight = true;
+        myAnimator.SetBool("changeDimension", false);
+    }
+}
--- Assets/Scripts/SwitchDimension.cs
+++ Assets/Scripts/SwitchDimension.cs
@@ -7,16 +7,25 @@ using UnityEngine.Tilemaps;
 public class SwitchDimension : MonoBehaviour
 {
     [SerializeField] Tilemap myTilemap;
+    [SerializeField] Color defaultTilemapColor;
     [SerializeField] Color backgroundColor;
+    [SerializeField] Color defaultDackgroundColor;
     [SerializeField] Color coinColor;
+    [SerializeField] Color defaultCoinColor;
     [SerializeField] Color playerColor;
+    [SerializeField] Color defaultPlayerColor;
     [SerializeField] Color spikeColor;
+    [SerializeField] Color defaultSpikeColor;
     [SerializeField] SpriteRenderer playerSpriteRenderer;
+
+    bool inLightDimension;
+    bool canSwitch;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        inLightDimension = true;
+        canSwitch = true;
     }
 
     // Update is called once per frame
@@ -28,8 +37,37 @@ public class SwitchDimension : MonoBehaviour
     //Used by the input system
     void OnSwitch(InputValue value)
     {
+        // if(inLightDimension)
+        //     ChangeToDarkDimension();
+        // else
+        //     ChangeToLightDimension();
+        if(canSwitch)
+        {
+            canSwitch = false;
+            FindObjectOfType<ScreenDarkener>().StartDarkeningAnimation();
+        }
+            
 
-        //Debug.Log("Do something");
+        //Switch();
+    }
+
+    public void ChangeDimension()
+    {
+        if (inLightDimension)
+            ChangeToDarkDimension();
+        else
+            ChangeToLightDimension();
+        
+    }
+
+    public void SetInLightDimension()
+    {
+        inLightDimension = !inLightDimension;
+        canSwitch = true;
+    }
+
+    private void ChangeToDarkDimension()
+    {
         myTilemap.color = Color.black;
         Background[] myBackgrounds = FindObjectsOfType<Background>();
         foreach (Background backgroound in myBackgrounds)
@@ -53,5 +91,32 @@ public class SwitchDimension : MonoBehaviour
         }
 
         playerSpriteRenderer.color = playerColor;
+    }
+
+    private void ChangeToLightDimension()
+    {
+        myTilemap.color = defaultTilemapColor;
+        Background[] myBackgrounds = FindObjectsOfType<Background>();
+        foreach (Background backgroound in myBackgrounds)
+        {
+            SpriteRenderer spriteRenderer = backgroound.gameObject.GetComponent<SpriteRenderer>();
+            spriteRenderer.color = defaultDackgroundColor;
+        }
+
+        Pickup[] myCoins = FindObjectsOfType<Pickup>();
+        foreach (Pickup coin in myCoins)
+        {
+            SpriteRenderer spriteRenderer = coin.gameObject.GetComponent<SpriteRenderer>();
+            spriteRenderer.color = defaultCoinColor;
+        }
+
+        Spike[] mySpikes = FindObjectsOfType<Spike>();
+        foreach (Spike spike in mySpikes)
+        {
+            SpriteRenderer spriteRenderer = spike.gameObject.GetComponent<SpriteRenderer>();
+            spriteRenderer.color = defaultSpikeColor;
+        }
+
+        playerSpriteRenderer.color = defaultPlayerColor;
     }
 }
